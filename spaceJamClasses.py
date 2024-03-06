d2052317c05d55824c037e246238629075add451
makaylaFar <159464385+makaylaFar@users.noreply.github.com>
2024-03-06 15:59:32 -0500

    first commit

---
+++ spaceJamClasses.py
@@ -0,0 +1,180 @@
+from panda3d.core import Vec3
+from panda3d.core import *
+from direct.showbase.ShowBase import ShowBase
+from direct.task import Task
+class Planet(ShowBase):
+    def __init__(self, loader: Loader, modelPath: str, parentNode: NodePath, nodeName: str, texPath: str, posVec: Vec3, scaleVec: float):
+        self.modelNode = loader.loadModel(modelPath)
+        self.modelNode.reparentTo(parentNode)
+        self.modelNode.setPos(posVec)
+        self.modelNode.setScale(scaleVec)
+
+        self.modelNode.setName(nodeName)
+        tex = loader.loadTexture(texPath)
+        self.modelNode.setTexture(tex, 1)
+
+class Drone(ShowBase):
+    droneCount = 0
+    def __init__(self, loader: Loader, modelPath: str, parentNode: NodePath, nodeName: str, texPath: str, posVec: Vec3, scaleVec: float):
+        self.modelNode = loader.loadModel(modelPath)
+        self.modelNode.reparentTo(parentNode)
+        self.modelNode.setPos(posVec)
+        self.modelNode.setScale(scaleVec)
+
+        self.modelNode.setName(nodeName)
+        tex = loader.loadTexture(texPath)
+        self.modelNode.setTexture(tex, 1)
+
+class universe(ShowBase):
+    def __init__(self, loader: Loader, modelPath: str, parentNode: NodePath, nodeName: str, texPath: str, posVec: Vec3, scaleVec: float):
+        self.modelNode = loader.loadModel(modelPath)
+        self.modelNode.reparentTo(parentNode)
+        self.modelNode.setPos(posVec)
+        self.modelNode.setScale(scaleVec)
+
+        self.modelNode.setName(nodeName)
+        tex = loader.loadTexture(texPath)
+        self.modelNode.setTexture(tex, 1)
+
+class spaceShip(ShowBase):
+    def __init__(self, loader: Loader, modelPath: str, parentNode: NodePath, nodeName: str, texPath: str, posVec: Vec3, scaleVec: float, task, render):
+        self.taskManager = task
+        self.render = render
+
+        self.modelNode = loader.loadModel(modelPath)
+        self.modelNode.reparentTo(parentNode)
+        self.modelNode.setPos(posVec)
+        self.modelNode.setScale(scaleVec)
+
+        self.modelNode.setName(nodeName)
+        tex = loader.loadTexture(texPath)
+        self.modelNode.setTexture(tex, 1)
+    def SetKeyBindings(self):
+        self.accept("w", self.Thrust, [1])
+        self.accept("w-up", self.Thrust, [0])
+
+        self.accept("a", self.LeftTurn, [1])
+        self.accept("a-up", self.LeftTurn, [0])
+
+        self.accept("d", self.RightTurn, [1])
+        self.accept("d-up", self.RightTurn, [0])
+
+        self.accept("shift", self.MoveUp, [1])
+        self.accept("shift-up", self.MoveUp, [0])
+
+        self.accept("enter", self.MoveDown, [1])
+        self.accept("enter-up", self.MoveDown, [0])
+
+        self.accept("arrow_left", self.RollLeft, [1])
+        self.accept("arrow_left-up", self.RollLeft, [0])
+
+        self.accept("arrow_right", self.RollRight, [1])
+        self.accept("arrow_right-up", self.RollRight, [0])
+
+    def Thrust(self, keyDown):
+        if keyDown:
+            self.taskManager.add(self.ApplyThrust, 'forward-thrust')
+        else:
+            self.taskManager.remove('forward-thrust')
+
+
+    def ApplyThrust(self, task):
+        rate = 5
+        trajectory = self.render.getRelativeVector(self.modelNode, Vec3.forward())
+        trajectory.normalize()
+        self.modelNode.setFluidPos(self.modelNode.getPos() + trajectory * rate)
+        return Task.cont
+
+    def LeftTurn(self, keyDown):
+        if keyDown:
+            self.taskManager.add(self.ApplyLeftTurn, 'left-turn')
+
+        else:
+            self.taskManager.remove('left-turn')
+    
+    def ApplyLeftTurn(self, task):
+        #half a degree every frame
+        rate = .5
+        self.modelNode.setH(self.modelNode.getH() + rate)
+        return Task.cont
+    
+    def RightTurn(self, keyDown):
+        if keyDown:
+            self.taskManager.add(self.ApplyRightTurn, 'right-turn')
+
+        else:
+            self.taskManager.remove('right-turn')
+    
+    def ApplyRightTurn(self, task):
+        #half a degree every frame
+        rate = -0.5
+        self.modelNode.setH(self.modelNode.getH() + rate)
+        return Task.cont
+    
+    def MoveUp(self, keyDown):
+        if keyDown:
+            self.taskManager.add(self.ApplyMoveUp, 'upward-thrust')
+        else:
+            self.taskManager.remove('upward-thrust')
+
+    
+    def ApplyMoveUp(self, task):
+        rate = .6
+        self.modelNode.setP(self.modelNode.getP() + rate)
+        return Task.cont
+    
+    def MoveDown(self, keyDown):
+        if keyDown:
+            self.taskManager.add(self.ApplyMoveDown, 'downward-thrust')
+        else:
+            self.taskManager.remove('downward-thrust')
+
+    
+    def ApplyMoveDown(self, task):
+        rate = -.6
+        self.modelNode.setP(self.modelNode.getP() + rate)
+        return Task.cont
+    
+    def RollLeft(self, keyDown):
+        if keyDown:
+            self.taskManager.add(self.ApplyRollLeft, 'left-roll')
+        else:
+            self.taskManager.remove('left-roll')
+
+    
+    def ApplyRollLeft(self, task):
+        rate = .6
+        self.modelNode.setR(self.modelNode.getR() + rate)
+        return Task.cont
+    
+    def RollRight(self, keyDown):
+        if keyDown:
+            self.taskManager.add(self.ApplyRollRight, 'right-roll')
+        else:
+            self.taskManager.remove('right-roll')
+
+    
+    def ApplyRollRight(self, task):
+        rate = .6
+        self.modelNode.setR(self.modelNode.getR() - rate)
+        return Task.cont
+
+class spaceStation(ShowBase):
+    def __init__(self, loader: Loader, modelPath: str, parentNode: NodePath, nodeName: str, texPath: str, posVec: Vec3, scaleVec: float):
+        self.modelNode = loader.loadModel(modelPath)
+        self.modelNode.reparentTo(parentNode)
+        self.modelNode.setPos(posVec)
+        self.modelNode.setScale(scaleVec)
+
+        self.modelNode.setName(nodeName)
+        tex = loader.loadTexture(texPath)
+        self.modelNode.setTexture(tex, 1)
+
+class xz():
+    circleIncrement = 0
+
+class yz():
+    circleIncrement = 0
+
+class xy():
+    circleIncrement = 0
